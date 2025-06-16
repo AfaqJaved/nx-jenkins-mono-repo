801e9607df9df2dc496533d090d568138fbccfe3
afaqjaved <afaqjavedofficial@gmail.com>
2025-06-16 19:21:03 +0500

    added

--- packages/server/src/app.controller.spec.ts
+++ packages/server/src/app.controller.spec.ts
@@ -17,7 +17,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe(core as string);
+      expect(appController.getHello()).toBe('This is from core');
     });
   });
 });
